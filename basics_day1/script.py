24347a32409cdbda24452bd6fc5c64ec75558e28
yinchenhsieh <yin.c.hsieh@gmail.com>
2024-03-26 13:24:17 +0100

    added first day

---
+++ basics_day1/script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# print some strings
+print('Hello! Welcome to the Python Course')
+
+# be consistent with your use of quotations
+# you can print quotes inside another string by sandwiching them as follows
+somestr = "a string 'between' doublequotes"
+
+print(somestr)
+
+# difference between an integer and float
+ten_int = 10
+print(type(ten_int))
+ten_float = 10.0
+print(type(ten_float))
