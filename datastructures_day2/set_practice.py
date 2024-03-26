03c9b85c1bf76efcc8e54280ffb633f39f37eeb9
yinchenhsieh <yin.c.hsieh@gmail.com>
2024-03-26 19:05:06 +0100

    added day 2 exercises

---
+++ datastructures_day2/set_practice.py
@@ -0,0 +1,27 @@
+"""
+set_practice.py
+
+In this exercise you will practice with set methods
+
+Follow the instructions below
+"""
+
+# create a set of colors: red, green, blue
+
+
+# add the color yellow to this set using the add method
+
+
+# update the set with the colors purple and pink with the update method
+
+
+# make a set out of this list
+
+alist = [1, 1, 2, 3, 4, 5, 6, 6, 6]
+
+# remove the 6 from the set using the discard method
+
+
+# here are two sets, find their intersection, union, and difference
+setx = {'green', 'blue'}
+sety = {'blue', 'yellow'}
---
+++ datastructures_day2/tuple_practice.py
@@ -0,0 +1,18 @@
+"""
+tuple_practice.py
+
+Practice with tuples
+
+Follow the directions
+"""
+
+# make a tuple of three different data types, an integer, a float, and a string
+
+
+# unpack the tuple into variables n1, n2, and n3
+
+
+# add the boolean True to the tuple - is this the same tuple?
+
+
+# convert the tuple to a list 
