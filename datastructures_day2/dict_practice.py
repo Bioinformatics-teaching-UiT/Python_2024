e6f4811cd89e15ea8234b43b14e8ba10ecce94ce
yinchenhsieh <yin.c.hsieh@gmail.com>
2024-03-26 13:28:00 +0100

    added day 2

---
+++ datastructures_day2/dict_practice.py
@@ -0,0 +1,37 @@
+"""
+dict_practice.py
+
+In this exercise you will practice with dictionary methods
+
+Follow the instructions below
+"""
+
+# make a dictionary that called number_names that stores as 
+# keys the numbers from 1 to 5 in words, and as values the integers themselves
+# so, an example of an entry would be where the key is 'two' and the value is 2
+
+
+# get the value corresponding to the key 'four'
+
+
+
+# get a list of all the keys in this dictionary
+
+
+# get a list of all the values in this dictionary
+
+
+# delete the last entry of the number_names
+
+
+
+# remove the 'two' entry
+
+
+
+# count the number of entries and print this out
+# again, do this programmatically! we are not testing if you can count ;)
+
+
+# delete all the values from the dictionary, what do you get?
+
---
+++ datastructures_day2/list_practice.py
@@ -0,0 +1,37 @@
+"""
+list_practice.py
+
+In this exercise, you will practice the most common list methods.
+
+Write your lines of code under each comment.
+"""
+
+# create a list named 'numbers' with the integers 1 to 8
+
+# add 9 to the end of the list
+
+# add the extension to the end of your numbers list
+
+
+# print out an informative message that gives the number of 
+# elements in the list, and then the list itself
+# no hard coding! (don't count in your head and print it, 
+# have the code count for you)
+
+
+
+# here is a list, can you print out how many times 'dog' occurs?
+animals = ['cat', 'rat', 'mouse', 'dog', 'giraffe', 'dog']
+
+
+# insert a moose at the first position of the animals list
+
+# using the remove() method, remove 'dog' - 
+# check the list - is this the behaviour you expect?
+
+# pop out the 3rd element, what do you get?
+
+# reverse the animal list
+
+# clear the animal list, what do you get?
+
