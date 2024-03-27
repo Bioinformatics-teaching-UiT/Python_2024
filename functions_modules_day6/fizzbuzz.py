06c4e18e31880e1e739ccdbf7fc9d88d337e036b
yinchenhsieh <yin.c.hsieh@gmail.com>
2024-03-27 12:04:59 +0100

    added day 6

---
+++ functions_modules_day6/fizzbuzz.py
@@ -0,0 +1,13 @@
+"""
+FizzBuzz exercise
+
+Write a script that goes through the numbers 1 to 100.
+
+If the number is divisible by 3 and 5, output 'FizzBuzz'.
+
+If the number is divisible by 3 (but not 5), output 'Fizz'.
+
+If the number is divisible by 5 (but not 3), output 'Buzz'.
+
+Otherwise, just print out the number.
+"""
