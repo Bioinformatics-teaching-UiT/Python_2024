890222613e1032b298d866fce98a9b16c1718c10
Prof. Ines Heiland <ines.heiland@uit.no>
2024-04-11 14:35:12 +0200

    Update play_with_names.py
    Changed play with names

--- datastructures_day2/play_with_names.py
+++ datastructures_day2/play_with_names.py
@@ -5,11 +5,8 @@ Created on Mon Apr  8 12:25:19 2024
 
 @author: ihe027
 """
-
-
-name = input("Please enter your full name here:")
-names=name.split()
-name_entries=len(names[0])
-print(name_entries)
-
+"""
+Read your name using input() and change (for example read it from the back, 
+change it into uppercase, only print out first name, it and print out a greeting)
+"""
 
