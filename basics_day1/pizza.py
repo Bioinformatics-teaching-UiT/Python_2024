053b8cbe8ae8ea3f181d801c28e5f4317a5e44a4
Prof. Ines Heiland <ines.heiland@uit.no>
2024-04-11 14:37:34 +0200

    Update pizza.py

--- basics_day1/pizza.py
+++ basics_day1/pizza.py
@@ -7,23 +7,13 @@ Created on Mon Apr  8 10:10:39 2024
 """
 
 #define pi and print it
-pi = 22/7
-print(pi)
 
-#reading pizza radius z and height a
-print("What is radius of the pizza?")
 
-z=float(input())
-print(type(z))
-
-a=2
+#reading in pizza radius z and height a from commandline
 
 
 
 #calculating the volume V and print it
-V=pi*z*z*a
-print(V)
+
 
 #convert vi into an integer and print it
-Vint=int(V)
-print(Vint)
