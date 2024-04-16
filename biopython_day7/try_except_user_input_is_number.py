b3ee68b726ba461c26ae7820756b85fa275c5143
yinchenhsieh <yin.c.hsieh@gmail.com>
2024-04-16 09:08:27 +0200

    added input number exercise

---
+++ biopython_day7/try_except_user_input_is_number.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 28 09:58:51 2023
+
+Exercise 1: 
+    
+    Write a while loop that only accepts integer numbers.
+    Use the input function, and if the user inputs 
+    anything other than a number,
+    have the user try again.
+
+@author: yin
+"""
+
